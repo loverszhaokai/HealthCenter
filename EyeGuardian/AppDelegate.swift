d9393e36f734c9be9626d2100689fdc5f0209e6c
Kai Zhao <loverszhao@gmail.com>
2016-05-25 08:37:50 +0800

    Add last time progress

--- EyeGuardian/AppDelegate.swift
+++ EyeGuardian/AppDelegate.swift
@@ -16,10 +16,25 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBOutlet weak var startButton: NSButton!
     @IBOutlet weak var stopButton: NSButton!
     @IBOutlet weak var skipButton: NSButton!
+    @IBOutlet weak var breakNowButton: NSButton!
 
-    @IBOutlet weak var log_text: NSScrollView!
+    @IBOutlet weak var logText: NSScrollView!
     @IBOutlet weak var workTimeText: NSTextField!
     @IBOutlet weak var restTimeText: NSTextField!
+    
+    @IBOutlet weak var work_time_unit: NSPopUpButton!
+    @IBOutlet weak var rest_time_unit: NSPopUpButton!
+    
+    @IBOutlet weak var lastTimeLabel: NSTextField!
+    @IBOutlet weak var lastTimeProgress: NSProgressIndicator!
+    
+    let POPUP_SECONDS_INDEX : Int = 0
+    let POPUP_MINUTES_INDEX : Int = 1
+    let POPUP_HOURS_INDEX : Int = 2
+
+    let SECOND_MULTIPLE : Double = 1
+    let MINUTE_MULTIPLE : Double = 60
+    let HOUR_MULTIPLE : Double = 60 * 60
 
     let skipWindow = NSWindow()
     private var fakeWindows: [NSWindow] = [NSWindow]()
@@ -36,7 +51,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
         initSkipWindow()
         SetTimeIntervalFromUserDefault()
-        tc.start(log_text)
+        tc.start(logText, lastTimeProgress: lastTimeProgress, lastTimeLabel: lastTimeLabel)
         setStopButton()
     }
 
@@ -47,7 +62,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     @IBAction func startButtonClick(sender: AnyObject) {
         NSLog("AppDelegate::startButtonClick()")
         SetTimeInterval()
-        tc.start(log_text)
+        tc.start(logText, lastTimeProgress: lastTimeProgress, lastTimeLabel: lastTimeLabel)
         setStopButton()
     }
     
@@ -70,10 +85,17 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         
         work()
         tc.stop()
-        tc.start(log_text)
+        tc.start(logText, lastTimeProgress: lastTimeProgress, lastTimeLabel: lastTimeLabel)
         setStopButton()
     }
     
+    @IBAction func breakNowClick(sender: AnyObject) {
+        NSLog("AppDelegate::breakNowClicked()")
+        
+        tc.stop()
+        tc.startToRest(logText, lastTimeProgress: lastTimeProgress, lastTimeLabel: lastTimeLabel)
+    }
+
     func setStartButton() {
         stopButton.enabled = false
         startButton.enabled = true
@@ -83,36 +105,36 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         startButton.enabled = false
         stopButton.enabled = true
     }
-    
+
     func SetTimeInterval() {
-        NSLog("AppDelegate::GetTimeInterval()")
+        NSLog("AppDelegate::SetTimeInterval()")
         NSLog("work_time_text=\(workTimeText.stringValue)")
         NSLog("rest_time_text=\(restTimeText.stringValue)")
         let work_time_interval : Double = Double(workTimeText.stringValue)!
         let rest_time_interval : Double = Double(restTimeText.stringValue)!
         
         if (work_time_interval > 0) {
-            tc.work_time_interval = work_time_interval
+            tc.work_time_interval = work_time_interval * Double(getTimesByIndexOfSelectedItem(work_time_unit.indexOfSelectedItem))
         }
-
         if (rest_time_interval > 0) {
-            tc.rest_time_interval = rest_time_interval
+            tc.rest_time_interval = rest_time_interval * Double(getTimesByIndexOfSelectedItem(rest_time_unit.indexOfSelectedItem))
         }
     }
     
     private func SetTimeIntervalFromUserDefault() {
-        let work_time_interval = NSUserDefaults.standardUserDefaults().floatForKey("work_time_interval")
-        let rest_time_interval = NSUserDefaults.standardUserDefaults().floatForKey("rest_time_interval")
+        // work_time_interval is seconds
+        let work_time_interval = NSUserDefaults.standardUserDefaults().doubleForKey("work_time_interval")
+        let rest_time_interval = NSUserDefaults.standardUserDefaults().doubleForKey("rest_time_interval")
 
         if (work_time_interval > 0) {
             tc.work_time_interval = NSTimeInterval(work_time_interval)
         }
-        workTimeText.stringValue = String(tc.work_time_interval);
-        
+        initTimeFieldAndPopUp(tc.work_time_interval, field: workTimeText, popUp: work_time_unit)
+
         if (rest_time_interval > 0) {
             tc.rest_time_interval = NSTimeInterval(rest_time_interval)
         }
-        restTimeText.stringValue = String(tc.rest_time_interval)
+        initTimeFieldAndPopUp(tc.rest_time_interval, field: restTimeText, popUp: rest_time_unit)
         
         NSLog(">>>> [config] work_time_interval=\(work_time_interval), rest_time_interval=\(rest_time_interval)")
     }
@@ -133,6 +155,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
         skipButton.setFrameOrigin(NSPoint(x: skipXPos, y: skipYPos))
         skipWindow.contentView?.addSubview(skipButton)
+        skipWindow.level = Int(CGWindowLevelForKey(.StatusWindowLevelKey)) + 1
     }
     
     func rest() {
@@ -156,6 +179,9 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             window.styleMask = NSBorderlessWindowMask
             window.setFrame(frame, display: true)
             window.makeKeyAndOrderFront(window)
+            window.backgroundColor = NSColor(deviceRed: 0.5, green: 0.5, blue: 0.5, alpha: 0.6)
+            window.opaque = false
+            window.toggleFullScreen(window)
             fakeWindows.append(window)
         }
     }
@@ -168,6 +194,33 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                 self.fakeWindows.removeAtIndex(objIndex)
             }
         }
+    }
+    
+    func getTimesByIndexOfSelectedItem(index : Int) -> Double {
+        if (index == POPUP_SECONDS_INDEX) {
+            return SECOND_MULTIPLE
+        } else if (index == POPUP_MINUTES_INDEX) {
+            return MINUTE_MULTIPLE
+        } else if (index == POPUP_HOURS_INDEX) {
+            return HOUR_MULTIPLE
+        }
+        return 0
+    }
+    
+    func initTimeFieldAndPopUp(timeInterval : Double, field : NSTextField, popUp : NSPopUpButton) {
+        var value = timeInterval
+        if (timeInterval > HOUR_MULTIPLE) {
+            value = Double(timeInterval) / Double(HOUR_MULTIPLE)
+            popUp.selectItemAtIndex(POPUP_HOURS_INDEX)
+        } else if (timeInterval > MINUTE_MULTIPLE) {
+            value = Double(timeInterval) / Double(MINUTE_MULTIPLE)
+            popUp.selectItemAtIndex(POPUP_MINUTES_INDEX)
+        } else {
+            value = Double(timeInterval) / Double(SECOND_MULTIPLE)
+            popUp.selectItemAtIndex(POPUP_SECONDS_INDEX)
+        }
+        field.stringValue = String(value)
+        field.sizeToFit()
     }
 }
 
--- EyeGuardian/TimerControl.swift
+++ EyeGuardian/TimerControl.swift
@@ -9,11 +9,17 @@
 import Cocoa
 
 class TimerControl: NSObject {
+    
+    let LAST_TIME_INTERVAL : NSTimeInterval = 1
 
-    private var _work_time_interval : NSTimeInterval = 5
-    private var _rest_time_interval : NSTimeInterval = 2
+    private var _work_time_interval : NSTimeInterval = 50 * 60
+    private var _rest_time_interval : NSTimeInterval = 5 * 60
     private var work_timer : NSTimer = NSTimer()
     private var rest_timer : NSTimer = NSTimer()
+    private var last_time_timer : NSTimer = NSTimer()
+    
+    private var last_current_time : Double = 0
+    private var last_total_time : Double = 0
     
     var work_time_interval: NSTimeInterval {
         get {
@@ -35,23 +41,33 @@ class TimerControl: NSObject {
         }
     }
 
-    func start(sv : NSScrollView) {
+    func start(sv : NSScrollView, lastTimeProgress : NSProgressIndicator, lastTimeLabel : NSTextField) {
         NSLog("TimerControl::start()")
-
-        work_timer = NSTimer.scheduledTimerWithTimeInterval(_work_time_interval, target: self, selector: "restStart:", userInfo: ["sv": sv], repeats: false)
+        updateLastTimeProgressAndLabel(lastTimeProgress, lastTimeLabel: lastTimeLabel, isWork: true)
+        work_timer = NSTimer.scheduledTimerWithTimeInterval(_work_time_interval, target: self, selector: "restStart:", userInfo: ["sv": sv, "lastTimeProgress" : lastTimeProgress, "lastTimeLabel" : lastTimeLabel], repeats: false)
         addToScrollView(sv, text: "TimerControl log:\n")
         addToScrollView(sv, text: "\t >> start to work, _work_time_interval=\(_work_time_interval)\n")
+    }
+    
+    func startToRest(sv : NSScrollView, lastTimeProgress : NSProgressIndicator, lastTimeLabel : NSTextField) {
+        updateLastTimeProgressAndLabel(lastTimeProgress, lastTimeLabel: lastTimeLabel, isWork: false)
+        rest_timer = NSTimer.scheduledTimerWithTimeInterval(_rest_time_interval, target: self, selector: "workStart:", userInfo: ["sv": sv, "lastTimeProgress" : lastTimeProgress, "lastTimeLabel" : lastTimeLabel], repeats: false)
+        addToScrollView(sv, text: "\t << start to rest, _rest_time_interval=\(_rest_time_interval)\n\n")
+        let appDelegate = NSApplication.sharedApplication().delegate as! AppDelegate
+        appDelegate.rest()
     }
     
     func stop() {
         work_timer.invalidate()
         rest_timer.invalidate()
+        last_time_timer.invalidate()
     }
 
     func workStart(timer: NSTimer) {
         NSLog("TimerControl::workStart(timer) start to work")
         let dict = timer.userInfo as! NSDictionary
-        work_timer = NSTimer.scheduledTimerWithTimeInterval(_work_time_interval, target: self, selector: "restStart:", userInfo: ["sv": dict["sv"]!], repeats: false)
+        updateLastTimeProgressAndLabel(dict["lastTimeProgress"] as! NSProgressIndicator, lastTimeLabel: dict["lastTimeLabel"] as! NSTextField, isWork: true)
+        work_timer = NSTimer.scheduledTimerWithTimeInterval(_work_time_interval, target: self, selector: "restStart:", userInfo: ["sv": dict["sv"]!, "lastTimeProgress" : dict["lastTimeProgress"]!, "lastTimeLabel" : dict["lastTimeLabel"]!], repeats: false)
         addToScrollView(dict["sv"] as! NSScrollView, text: "\t >> start to work, _work_time_interval=\(_work_time_interval)\n")
         let appDelegate = NSApplication.sharedApplication().delegate as! AppDelegate
         appDelegate.work()
@@ -60,8 +76,8 @@ class TimerControl: NSObject {
     func restStart(timer: NSTimer) {
         NSLog("TimerControl::restStart(timer) start to rest")
         let dict = timer.userInfo as! NSDictionary
-        rest_timer = NSTimer.scheduledTimerWithTimeInterval(_rest_time_interval, target: self, selector: "workStart:", userInfo: ["sv": dict["sv"]!], repeats: false)
-        NSRunLoop.currentRunLoop().addTimer(rest_timer, forMode: NSRunLoopCommonModes)
+        updateLastTimeProgressAndLabel(dict["lastTimeProgress"] as! NSProgressIndicator, lastTimeLabel: dict["lastTimeLabel"] as! NSTextField, isWork: false)
+        rest_timer = NSTimer.scheduledTimerWithTimeInterval(_rest_time_interval, target: self, selector: "workStart:", userInfo: ["sv": dict["sv"]!, "lastTimeProgress" : dict["lastTimeProgress"]!, "lastTimeLabel" : dict["lastTimeLabel"]!], repeats: false)
         addToScrollView(dict["sv"] as! NSScrollView, text: "\t << start to rest, _rest_time_interval=\(_rest_time_interval)\n\n")
         let appDelegate = NSApplication.sharedApplication().delegate as! AppDelegate
         appDelegate.rest()
@@ -70,5 +86,34 @@ class TimerControl: NSObject {
     func addToScrollView(sv: NSScrollView, text: String) {
         let textView = sv.documentView as! NSTextView
         textView.string?.appendContentsOf(text)
+    }
+    
+    func updateLastTimeProgressAndLabel(lastTimeProgress : NSProgressIndicator, lastTimeLabel : NSTextField, isWork : Bool) {
+        last_current_time = 0
+        if (isWork) {
+            last_total_time = _work_time_interval
+        } else {
+            last_total_time = _rest_time_interval
+        }
+        last_time_timer.invalidate()
+        last_time_timer = NSTimer.scheduledTimerWithTimeInterval(LAST_TIME_INTERVAL, target: self, selector: "updateLastTimeProgress:", userInfo: ["lastTimeProgress" : lastTimeProgress, "lastTimeLabel" : lastTimeLabel, "isWork" : isWork], repeats: true)
+    }
+    
+    func updateLastTimeProgress(timer : NSTimer) {
+        last_current_time++
+        NSLog("\t updateLastTimeProgress: last_current_time=\(last_current_time)  last_total_time=\(last_total_time) doublev=\(last_current_time / last_total_time)")
+        let dict = timer.userInfo as! NSDictionary
+        let lastTimeProgress = dict["lastTimeProgress"] as! NSProgressIndicator
+        let lastTimeLabel = dict["lastTimeLabel"] as! NSTextField
+        let isWork = dict["isWork"] as! Bool
+        lastTimeProgress.doubleValue = 100 * last_current_time / last_total_time
+        var text : String = String(last_total_time - last_current_time) + " seconds until"
+        if (isWork) {
+            text.appendContentsOf(" rest")
+        } else {
+            text.appendContentsOf(" work")
+        }
+        lastTimeLabel.stringValue = text
+        lastTimeLabel.sizeToFit()
     }
 }
